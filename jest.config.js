182d216876a5f9cd346eec45f0480523349fac87
ljacks98 <ljacks98@eagles.nccu.edu>
2026-01-25 18:30:23 -0500

    Add jest.config.js file

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  preset: 'react-native',
+  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
+  transformIgnorePatterns: [
+    'node_modules/(?!(react-native|@react-native|expo|@expo|@react-navigation)/)',
+  ],
+  testMatch: ['**/__tests__/**/*.js', '**/?(*.)+(spec|test).js'],
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx}',
+    '!src/**/*.test.{js,jsx}',
+    '!src/index.js',
+  ],
+};
